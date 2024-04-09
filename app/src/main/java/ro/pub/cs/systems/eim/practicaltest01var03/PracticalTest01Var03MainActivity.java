f4fb8b278cd156bcdf9ff4aedc32a23ae08fd975
antra-cet <andra.bivolaru@stud.acs.upb.ro>
2024-04-09 14:20:24 +0300

    exB1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -1,6 +1,10 @@
 package ro.pub.cs.systems.eim.practicaltest01var03;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,32 @@ public class PracticalTest01Var03MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practial_test01_var03_main);
+
+        // Buttons
+        Button plus = (Button) findViewById(R.id.plus);
+        Button minus = (Button) findViewById(R.id.minus);
+        Button navigateToSecondaryActivityButton = (Button) findViewById(R.id.navigate_to_secondary_activity);
+
+        // Edit texts
+        EditText text1 = (EditText) findViewById(R.id.text1);
+        EditText text2 = (EditText) findViewById(R.id.text2);
+        TextView text3 = (TextView) findViewById(R.id.sum);
+
+        // If buttons plus or minus are pressed then the sum is calculated but verify if edit text contain integer numbers, if not display a toast message
+        plus.setOnClickListener(v -> {
+            if (text1.getText().toString().matches("\\d+") && text2.getText().toString().matches("\\d+")) {
+                text3.setText(text1.getText().toString() + " + " + text2.getText().toString() + " = " + String.valueOf(Integer.parseInt(text1.getText().toString()) + Integer.parseInt(text2.getText().toString())));
+            } else {
+                Toast.makeText(this, "Text fields must contain integer numbers", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        minus.setOnClickListener(v -> {
+            if (text1.getText().toString().matches("\\d+") && text2.getText().toString().matches("\\d+")) {
+                text3.setText(text1.getText().toString() + " - " + text2.getText().toString() + " = " + String.valueOf(Integer.parseInt(text1.getText().toString()) - Integer.parseInt(text2.getText().toString())));
+            } else {
+                Toast.makeText(this, "Text fields must contain integer numbers", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
